5f3c531484b0ff09fad6f1268a928087be39b5d4
Heather Praiswater <heathered@gmail.com>
2014-09-11 13:12:29 -0500

    Removed unnecessary method.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,17 +1,5 @@
 class Like < ActiveRecord::Base
   attr_accessible :article_id, :ip_address
   
-  def liked?
-    all = Like.all
-    
-    all.each do |a|
-      if a.ip_address == request.remote_ip
-        true
-      else
-        false
-      end
-    end
-  end
-  
   
 end
